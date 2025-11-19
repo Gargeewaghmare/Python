85dfc316b609691997dc6c59a36aa89ea24541a8
Gargeewaghmare <gargeew28@gmail.com>
2025-11-19 23:45:52 +0530

    Add files via upload

---
+++ dictionaryaccessing.py
@@ -0,0 +1,4 @@
+student_info = {"name":"Gargee", "age":"18", "course":"Computer Science and Bussines System", "is_enrolled":True}
+print(student_info)
+print(student_info["name"])
+print(student_info.get("age"))
