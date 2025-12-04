f8bb66bec1534d62186320a245f548f49243ad6d
Gargeewaghmare <gargeew28@gmail.com>
2025-12-04 14:17:04 +0530

    Add files via upload

---
+++ numnature.py
@@ -0,0 +1,17 @@
+def number(n):
+    if (n>0):
+        print("Positive")
+    elif(n<0):
+        print("Negative")
+    elif(n == 0):
+        print("Zero")
+    else:
+        print("default")
+number(-3)
+
+
+
+            
+    
+
+
